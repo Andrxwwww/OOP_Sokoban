3951d3f8ccc84f1c51edb289df07ec1ea12fafb7
Andrxwwww <117539407+Andrxwwww@users.noreply.github.com>
2024-02-09 22:28:34 +0000

    commit for sukoban

---
+++ src/pt/iscte/poo/sokoban/Bateria.java
@@ -0,0 +1,22 @@
+package pt.iscte.poo.sokoban;
+
+import pt.iscte.poo.utils.Point2D;  
+
+public class Bateria extends GameElement{
+
+    public Bateria(Point2D position) {
+        super(position);
+    }
+
+    @Override
+    public String getName() {
+        return "Bateria";
+    }
+
+    @Override
+    public int getLayer() {
+        return 1;
+    }
+
+
+}
---
+++ src/pt/iscte/poo/sokoban/Interactable.java
@@ -0,0 +1,8 @@
+package pt.iscte.poo.sokoban;
+
+public interface Interactable {
+
+    //Interface que permite a interação entre elementos do jogo
+    public void interactWith(GameElement element);
+
+}

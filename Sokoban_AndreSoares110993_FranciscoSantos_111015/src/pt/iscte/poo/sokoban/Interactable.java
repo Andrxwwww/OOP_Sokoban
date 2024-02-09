2574e70e54fdde7ef2555e5cfea07024e063231c
Andrxwwww <117539407+Andrxwwww@users.noreply.github.com>
2024-02-09 22:25:53 +0000

    commit for sukoban

--- Sokoban_AndreSoares110993_FranciscoSantos_111015/src/pt/iscte/poo/sokoban/Interactable.java
+++
@@ -1,8 +0,0 @@
-package pt.iscte.poo.sokoban;
-
-public interface Interactable {
-
-    //Interface que permite a interação entre elementos do jogo
-    public void interactWith(GameElement element);
-
-}
